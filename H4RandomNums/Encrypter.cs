7993bfa9f719ec262e4409f4ad08343a1e750cc3
Jepsen <Jepsen@DESKTOP-HT94JHQ>
2022-11-07 12:36:42 +0100

    Add project files.

---
+++ H4RandomNums/Encrypter.cs
@@ -0,0 +1,42 @@
+﻿namespace H4RandomNums
+{
+    public class Encrypter
+    {
+
+        string alphabet = "ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ";
+        string enalphabet = "KLMNOPQRSTUVXYZÆØÅABCDEFGHIJ";
+        public Encrypter()
+        {
+        }
+        public void Encrypt(string text)
+        {
+            string encrypted = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (Char.ToLower(text[i]) == Char.ToLower(alphabet[j]))
+                    {
+                        encrypted += enalphabet[j + 1];
+                    }
+                }
+            }
+            Console.WriteLine(encrypted);
+        }
+        public void Decrypt(string text)
+        {
+            string decrypted = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < enalphabet.Length; j++)
+                {
+                    if (Char.ToLower(text[i]) == Char.ToLower(enalphabet[j]))
+                    {
+                        decrypted += alphabet[j -1];
+                    }
+                }
+            }
+            Console.WriteLine(decrypted);
+        }
+    }
+}
---
+++ H4RandomNums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace H4RandomNums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encrypter encrypter = new Encrypter();
+            encrypter.Encrypt("Hello");
+            encrypter.Decrypt("SPXXÆ");
+            Console.ReadKey();
+        }
+        
+    }
+}
